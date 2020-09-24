a8d3b26f78796d6abcf98a04c14de00124830f8f
hiro.bata <kawa.060313@gmail.com>
2020-09-24 08:33:32 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,19 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   
   def index
-    @tasks = Task.order(date: :asc)
+    @tasks = current_user.tasks.order(date: :desc)
   end
 
   def new
     @task = Task.new
+    
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "記録されました。"
@@ -34,6 +36,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :date, :hour, :minute)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url  
+    end
   end
   
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+    validates :content, presence: true, length: { maximum: 15 }
+    validates :date, presence: true, length: { maximum: 15 }
+    
+    belongs_to :user
 end
